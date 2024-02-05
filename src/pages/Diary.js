5f548b50d8d34d75d89b533b1ecb868be1a559de
sueWavy <sueboi0311@gmail.com>
2024-02-06 00:13:06 +0900

    Learn : useParams, Query String, useNavigate

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function Diary() {
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div>
       <p>여기는 DIARY</p>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Edit() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
+  console.log("mode : ", mode);
+  console.log("id : ", id);
+
+  const navigate = useNavigate();
+
   return (
     <div>
       <p>여기는 EDIT</p>
+      <button onClick={() => setSearchParams({ who: "soo" })}>QS 변경</button>
+      <button onClick={() => navigate("/home")}>HOME</button>
+      <button onClick={() => navigate(-1)}>뒤로가기</button>
     </div>
   );
 }
